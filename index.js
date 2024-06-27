28e76032f56f2cdc74b9e59b82fee368290bb49a
code-iddih <idinyamwaya@gmail.com>
2024-06-27 13:42:45 +0300

    Completes the Tasks

--- index.js
+++ index.js
@@ -1,1 +1,27 @@
 // Write your solution in this file!
+
+// Customer Name
+
+var customerName = "bob";
+
+const leastFavoriteCustomer = "Another Bob";
+
+function upperCaseCustomerName() {
+
+    customerName = customerName.toUpperCase(); //Converts the charaters to UPPERCASE
+}
+
+function setBestCustomer() {
+
+    bestCustomer = "not bob";
+}
+
+function overwriteBestCustomer() {
+
+    bestCustomer = "maybe bob";
+}
+
+function changeLeastFavoriteCustomer() {
+
+    leastFavoriteCustomer = "Changed Favorite Customer";
+}
